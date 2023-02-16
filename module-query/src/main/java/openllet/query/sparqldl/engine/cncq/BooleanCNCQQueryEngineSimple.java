5642be72ae3adbb1625557cbe5a887e06fe56786
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-02-16 16:05:33 +0100

    Adds classes for tracking changes in ABox for CNCQ engine
    Needed because rolling back (i.e. cleaning up ABox of query KB) is required after checking satisfiability

--- module-query/src/main/java/openllet/query/sparqldl/engine/cncq/BooleanCNCQQueryEngineSimple.java
+++ module-query/src/main/java/openllet/query/sparqldl/engine/cncq/BooleanCNCQQueryEngineSimple.java
@@ -5,6 +5,7 @@ import openllet.core.DependencySet;
 import openllet.core.KnowledgeBase;
 import openllet.core.boxes.abox.ABox;
 import openllet.core.boxes.abox.Individual;
+import openllet.core.utils.ATermUtils;
 import openllet.query.sparqldl.engine.AbstractBooleanQueryEngine;
 import openllet.query.sparqldl.engine.ucq.UnionQueryEngineSimple;
 import openllet.query.sparqldl.model.AtomQuery;
@@ -14,6 +15,7 @@ import openllet.query.sparqldl.model.cq.QueryAtom;
 import openllet.query.sparqldl.model.ucq.UnionQuery;
 import openllet.query.sparqldl.model.ucq.UnionQueryImpl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,15 +25,102 @@ import java.util.Map;
  */
 public class BooleanCNCQQueryEngineSimple extends AbstractBooleanQueryEngine<CNCQQuery>
 {
+    private abstract class Change
+    {
+    }
+    private class TypeChange extends Change
+    {
+        private final ATermAppl _ind;
+        private final ATermAppl _type;
+
+        TypeChange(ATermAppl ind, ATermAppl type)
+        {
+            _ind = ind;
+            _type = type;
+        }
+
+        protected ATermAppl getInd()
+        {
+            return _ind;
+        }
+
+        protected ATermAppl getType()
+        {
+            return _type;
+        }
+
+        @Override
+        public String toString() {
+            return _type + "(" + _type + ")";
+        }
+    }
+    private class PropertyChange extends Change
+    {
+        private final ATermAppl _subj;
+        private final ATermAppl _pred;
+        private final ATermAppl _obj;
+
+        PropertyChange(ATermAppl subj, ATermAppl pred, ATermAppl obj)
+        {
+            _subj = subj;
+            _pred = pred;
+            _obj = obj;
+        }
+
+        protected ATermAppl getSubj()
+        {
+            return _subj;
+        }
+
+        protected ATermAppl getPred()
+        {
+            return _pred;
+        }
+
+        protected ATermAppl getObj()
+        {
+            return _obj;
+        }
+
+        @Override
+        public String toString() {
+            return _pred + "(" + _subj + ", " + _obj + ")";
+        }
+    }
+
+    private class FreshIndChange extends Change
+    {
+        private final ATermAppl _ind;
+
+        FreshIndChange(ATermAppl ind)
+        {
+            _ind = ind;
+        }
+
+        protected ATermAppl getInd()
+        {
+            return _ind;
+        }
+
+        @Override
+        public String toString() {
+            return _ind.toString();
+        }
+    }
+
     private UnionQueryEngineSimple _ucqEngine = new UnionQueryEngineSimple();
     private boolean _rollUpBeforeChecking = false;
     private Map<ATermAppl, ATermAppl> _queryVarsToFreshInds = new HashMap<>();
+    private List<Change> _changes = new ArrayList<>();
+    private ABox _abox = null;
+    private int _freshIndCounter = 0;
 
     @Override
     protected boolean execBooleanABoxQuery(CNCQQuery q)
     {
         // 1. PRELIMINARY CONSISTENCY CHECK
         q.getKB().ensureConsistency();
+        _abox = q.getKB().getABox();
 
         // 2. SEPARATE POSITIVE AND NEGATIVE PART & MERGE POSITIVE PART
         ConjunctiveQuery positiveQuery = q.mergePositiveQueries();
@@ -42,14 +131,15 @@ public class BooleanCNCQQueryEngineSimple extends AbstractBooleanQueryEngine<CNC
             positiveQuery = positiveQuery.splitAndRollUp(false);
 
         // 3. PUT POSITIVE ATOMS IN A-BOX
-        ABox modifiedABox = putQueryAtomsInABox(positiveQuery, q.getKB());
+        putQueryAtomsInABox(positiveQuery);
 
         // 4. QUERY IS NOT SATISFIABLE IF KB IS INCONSISTENT
-        if (!modifiedABox.isConsistent())
+        if (!q.getKB().isConsistent())
             return false;
 
         // 5. CHECK FOR SATISFIABILITY
-        boolean isSat = isSatisfied(negativeQueries, modifiedABox.getKB(), q.isDistinct());
+        // TODO Lukas: modifiedABox.getKB() is wrong - we need to set the ABox here - otherwise original ABox is used...
+        boolean isSat = isSatisfied(negativeQueries, q.getKB(), q.isDistinct());
 
         // 6. CLEAN-UP
         cleanUp();
@@ -57,30 +147,30 @@ public class BooleanCNCQQueryEngineSimple extends AbstractBooleanQueryEngine<CNC
         return isSat;
     }
 
-    private ABox putQueryAtomsInABox(AtomQuery<?> query, KnowledgeBase kb)
+    private void putQueryAtomsInABox(AtomQuery<?> query)
     {
-        final ABox copy = kb.getABox().copy();
         for (QueryAtom atom : query.getAtoms())
         {
             switch (atom.getPredicate())
             {
                 case Type ->
                 {
-                    ATermAppl var = getIndividual(atom.getArguments().get(0), copy);
+                    ATermAppl var = getIndividual(atom.getArguments().get(0), _abox);
                     ATermAppl type = atom.getArguments().get(1);
-                    copy.addType(var, type);
+                    _abox.addType(var, type);
+                    _changes.add(new TypeChange(var, type));
                 }
                 case PropertyValue ->
                 {
-                    ATermAppl subj = getIndividual(atom.getArguments().get(0), copy);
+                    ATermAppl subj = getIndividual(atom.getArguments().get(0), _abox);
                     ATermAppl pred = atom.getArguments().get(1);
-                    ATermAppl obj = getIndividual(atom.getArguments().get(2), copy);
-                    copy.addEdge(pred, subj, obj, DependencySet.INDEPENDENT);
+                    ATermAppl obj = getIndividual(atom.getArguments().get(2), _abox);
+                    _abox.addEdge(pred, subj, obj, DependencySet.INDEPENDENT);
+                    _changes.add(new PropertyChange(subj, pred, obj));
                 }
                 default -> _logger.warning("Encountered query predicate that is not supported: " + atom.getPredicate());
             }
         }
-        return copy;
     }
 
     private ATermAppl getIndividual(ATermAppl var, ABox abox)
@@ -89,8 +179,13 @@ public class BooleanCNCQQueryEngineSimple extends AbstractBooleanQueryEngine<CNC
         if (!abox.getKB().isIndividual(var))
         {
             if (!_queryVarsToFreshInds.containsKey(var))
-                _queryVarsToFreshInds.put(var, // TODO Lukas: null here?
-                        abox.addFreshIndividual(null, DependencySet.INDEPENDENT).getTerm());
+            {
+                Individual ind = abox.getKB().addIndividual(ATermUtils.makeTermAppl("__NEW_CNCQ_IND_" + _freshIndCounter));
+                ATermAppl freshInd = ind.getTerm();
+                _freshIndCounter++;
+                _queryVarsToFreshInds.put(var, freshInd);
+                _changes.add(new FreshIndChange(freshInd));
+            }
             res = _queryVarsToFreshInds.get(var);
         }
         return res;
@@ -105,13 +200,22 @@ public class BooleanCNCQQueryEngineSimple extends AbstractBooleanQueryEngine<CNC
             positiveQuery.setNegation(false);
             ucq.addQuery(positiveQuery);
         }
-        System.out.println(ucq);
         // No need to set result / dist. variables for the UCQ in Boolean engine
         return _ucqEngine.exec(ucq).isEmpty();
     }
 
     private void cleanUp()
     {
+        _changes.sort((c1, c2) -> c1 instanceof FreshIndChange && !(c2 instanceof FreshIndChange) ? 1 :
+                !(c1 instanceof FreshIndChange) && c2 instanceof FreshIndChange ? -1 : 0);
+        for (Change change : _changes)
+            if (change instanceof TypeChange c)
+                _abox.getKB().removeType(c.getInd(), c.getType());
+            else if (change instanceof PropertyChange c)
+                _abox.getKB().removePropertyValue(c.getPred(), c.getSubj(), c.getObj());
+            else if (change instanceof FreshIndChange c)
+                _abox.removeNode(c.getInd());
         _queryVarsToFreshInds = new HashMap<>();
+        _changes = new ArrayList<>();
     }
 }
