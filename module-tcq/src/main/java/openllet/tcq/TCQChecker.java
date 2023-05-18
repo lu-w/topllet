3212f1dabdd1dcb9694964d90de881146b1871dc
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-05-18 13:35:03 +0200

    Adds a simple entry functionality to access the TCQ module
    This is just for testing and will be removed in later versions. Plan is to integrate this into openllet's CLI app.

---
+++ module-tcq/src/main/java/openllet/tcq/TCQChecker.java
@@ -0,0 +1,32 @@
+package openllet.tcq;
+
+import java.util.Arrays;
+
+import openllet.tcq.engine.BooleanTCQEngine;
+import openllet.tcq.engine.BooleanTCQEngineImpl;
+import openllet.tcq.model.kb.TemporalKnowledgeBase;
+import openllet.tcq.model.kb.TemporalKnowledgeBaseImpl;
+import openllet.tcq.model.query.TemporalConjunctiveQuery;
+import openllet.tcq.model.query.TemporalConjunctiveQueryImpl;
+
+public class TCQChecker
+{
+    public static void main(final String[] args) {
+        if (args.length > 1)
+        {
+            // Import TBox & ABoxes
+            TemporalKnowledgeBase kb = new TemporalKnowledgeBaseImpl(
+                    Arrays.stream(args).toList().subList(1, args.length));
+
+            // Parse TCQ
+            TemporalConjunctiveQuery tcq = new TemporalConjunctiveQueryImpl(args[0], kb, false);
+
+            // Run BooleanTCQEngine
+            BooleanTCQEngine engine = new BooleanTCQEngineImpl();
+            boolean res = engine.exec(tcq);
+
+            // Output
+            System.out.println("Result: " + res);
+        }
+    }
+}
