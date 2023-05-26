67fa16f93014166ad0320a2d247ec20bd7f63484
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-05-26 10:09:52 +0200

    Fixes bug where ontologies were not correctly loaded
    Manager needs to be re-created every time

--- module-tcq/src/main/java/openllet/tcq/model/kb/loader/KnowledgeBaseLoader.java
+++ module-tcq/src/main/java/openllet/tcq/model/kb/loader/KnowledgeBaseLoader.java
@@ -1,51 +1,81 @@
 package openllet.tcq.model.kb.loader;
 
 import openllet.core.KnowledgeBase;
+import openllet.core.utils.Timer;
+import openllet.core.utils.Timers;
+import openllet.owlapi.OpenlletReasoner;
+import openllet.owlapi.OpenlletReasonerFactory;
 import openllet.shared.tools.Log;
 import org.protege.xmlcatalog.owlapi.XMLCatalogIRIMapper;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
-public abstract class KnowledgeBaseLoader
+public class KnowledgeBaseLoader
 {
-    public static final Logger _logger = Log.getLogger(ReloadKnowledgeBaseLoader.class);
+    public static final Logger _logger = Log.getLogger(KnowledgeBaseLoader.class);
+
+    protected static final String _defaultCatalogFileName = "catalog-v001.xml";
+    protected Set<OWLOntologyIRIMapper> _catalogs = new HashSet<>();
+    protected Timer _timer = new Timer("ontology loading");
+
+    public KnowledgeBaseLoader()
+    {
+    }
+
+    public KnowledgeBaseLoader(Timer timer)
+    {
+        if (timer != null)
+            _timer = timer;
+    }
 
     @Nullable
-    public abstract KnowledgeBase load(String fileName);
-
-    protected static OWLOntology loadOWLOntology(String fileName)
+    public KnowledgeBase load(String fileName) throws OWLOntologyCreationException, FileNotFoundException
     {
-        OWLOntology ont = null;
-        final String catalogFileName = "/catalog-v001.xml";
-        File catalog = new File(new File(fileName).getParent() + catalogFileName);
-        if (!catalog.exists())
-            catalog = new File(System.getProperty("user.dir") + catalogFileName);
+        _timer.start();
+        OWLOntology ont = loadOntology(fileName);
+        OpenlletReasoner reasoner = OpenlletReasonerFactory.getInstance().createReasoner(ont);
+        _timer.stop();
+        _logger.info("Loaded " + fileName);
+        return reasoner.getKB();
+    }
+
+    public void addCatalog(String catalogFileName)
+    {
+        if (catalogFileName != null)
+        {
+            try
+            {
+                File catalog = new File(catalogFileName);
+                XMLCatalogIRIMapper mapper = new XMLCatalogIRIMapper(catalog);
+                _catalogs.add(mapper);
+            }
+            catch (IOException e)
+            {
+                _logger.warning("Can not load catalog " + catalogFileName);
+            }
+        }
+    }
+
+    protected OWLOntology loadOntology(String fileName) throws OWLOntologyCreationException, FileNotFoundException
+    {
         OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-        man.getClass().getPackage().getImplementationVersion();
-        try
-        {
-            man.addIRIMapper(new XMLCatalogIRIMapper(catalog));
-        }
-        catch (IOException e)
-        {
-            _logger.info("Can not load catalog " + catalogFileName);
-        }
-        try
-        {
-             ont = man.loadOntology(IRI.create("file://" + fileName));
-        }
-        catch (OWLOntologyCreationException e)
-        {
-            _logger.warning("Can not load knowledge base from " + fileName);
-        }
-        return ont;
+        man.setIRIMappers(_catalogs);
+        String catalogFileString = new File(fileName).getParent() + "/" + _defaultCatalogFileName;
+        File catalogFile = new File(catalogFileString);
+        if (catalogFile.exists())
+            addCatalog(catalogFileString);
+        File file = new File(fileName);
+        if (file.exists())
+            return man.loadOntologyFromOntologyDocument(file);
+        else
+            throw new FileNotFoundException(file.toString());
     }
 }
