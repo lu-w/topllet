aec75410e86e4fca765b9821cde0bc6bebb7623b
Lukas Westhofen <lukas.westhofen@dlr.de>
2023-05-26 10:08:43 +0200

    Improves exception handling

--- module-tcq/src/main/java/openllet/tcq/model/kb/TemporalKnowledgeBaseImpl.java
+++ module-tcq/src/main/java/openllet/tcq/model/kb/TemporalKnowledgeBaseImpl.java
@@ -1,13 +1,15 @@
 package openllet.tcq.model.kb;
 
 import openllet.core.KnowledgeBase;
+import openllet.core.utils.Timer;
 import openllet.core.utils.iterator.IteratorUtils;
 import openllet.shared.tools.Log;
 import openllet.tcq.model.kb.loader.IncrementalKnowledgeBaseLoader;
 import openllet.tcq.model.kb.loader.KnowledgeBaseLoader;
-import openllet.tcq.model.kb.loader.ReloadKnowledgeBaseLoader;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 import javax.annotation.Nullable;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -23,22 +25,35 @@ public class TemporalKnowledgeBaseImpl implements TemporalKnowledgeBase
     private KnowledgeBaseLoader _loader;
     private final List<String> _files;
     private boolean _firstCalled = false;
+    private static final LoadingMode defaultLoadingMode = LoadingMode.INCREMENTAL;
 
     public TemporalKnowledgeBaseImpl(Iterable<String> files)
     {
-        this(files, LoadingMode.INCREMENTAL);
+        this(files, null, defaultLoadingMode);
     }
 
-    public TemporalKnowledgeBaseImpl(Iterable<String> files, LoadingMode loadingMode)
+    public TemporalKnowledgeBaseImpl(Iterable<String> files, String catalogFile)
+    {
+        this(files, catalogFile, defaultLoadingMode);
+    }
+
+    public TemporalKnowledgeBaseImpl(Iterable<String> files, String catalogFile, LoadingMode loadingMode)
+    {
+        this(files, catalogFile, loadingMode, null);
+    }
+
+    public TemporalKnowledgeBaseImpl(Iterable<String> files, String catalogFile, LoadingMode loadingMode, Timer timer)
     {
         _files = IteratorUtils.toList(files.iterator());
         // TODO we can cleverly decide which loader to use based on the number of CNCQs estimated and the size of the
         //  ABox... high # CNCQs & small ABox -> inc loader
         switch (loadingMode)
         {
-            case DEFAULT -> _loader = new ReloadKnowledgeBaseLoader();
-            case INCREMENTAL -> _loader = new IncrementalKnowledgeBaseLoader();
+            case DEFAULT -> _loader = new KnowledgeBaseLoader(timer);
+            case INCREMENTAL -> _loader = new IncrementalKnowledgeBaseLoader(timer);
         }
+        if (catalogFile != null)
+            _loader.addCatalog(catalogFile);
     }
 
     @Override
@@ -54,7 +69,14 @@ public class TemporalKnowledgeBaseImpl implements TemporalKnowledgeBase
         if (hasNext() && !(_firstCalled && _curKB != null))
         {
             _curKBIndex++;
-            _curKB = _loader.load(_files.get(_curKBIndex));
+            try
+            {
+                _curKB = _loader.load(_files.get(_curKBIndex));
+            }
+            catch (OWLOntologyCreationException | FileNotFoundException e)
+            {
+                throw new RuntimeException(e);
+            }
         }
         _firstCalled = false;
         return _curKB;
@@ -67,7 +89,14 @@ public class TemporalKnowledgeBaseImpl implements TemporalKnowledgeBase
         {
             _curKBIndex = 0;
             _firstCalled = true;
-            _curKB = _loader.load(_files.get(0));
+            try
+            {
+                _curKB = _loader.load(_files.get(0));
+            }
+            catch (OWLOntologyCreationException | FileNotFoundException e)
+            {
+                throw new RuntimeException(e);
+            }
         }
         return _curKB;
     }
